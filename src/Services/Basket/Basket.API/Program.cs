ec7958658be8ffec300e23cddbbbefb07b4f362f
Darshil Shah <sdarshil786@gmail.com>
2024-12-16 23:36:23 +0530

    Added Carter and MediaTR in Program.cs

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
+
 var app = builder.Build();
 
-
+app.MapCarter();
 
 app.Run();
