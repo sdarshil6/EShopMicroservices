5d4e1bae6abc988805bc4671d9b666c0a1a9161d
Darshil Shah <sdarshil786@gmail.com>
2024-12-16 21:27:37 +0530

    adding health checks and final change in dockerr config before creation of BasketAPI

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,7 @@
-
-
-
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
 
 var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
@@ -27,10 +24,17 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", new HealthCheckOptions()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
