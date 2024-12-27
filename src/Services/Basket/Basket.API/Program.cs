b89003f67fe18d440a2e734717c787a4ef829249
Darshil Shah <sdarshil786@gmail.com>
2024-12-27 13:36:05 +0530

    Register and Configure Marten DocumentDB library into Program.cs asp.net DI, modify appsettings.json

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -9,6 +9,11 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
 });
 
+builder.Services.AddMarten(opts => {
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 app.MapCarter();
