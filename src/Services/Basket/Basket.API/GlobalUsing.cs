bc31faaeb7d29f945e80b0b53acd199aa9ff74bc
Darshil Shah <sdarshil786@gmail.com>
2024-12-27 13:26:03 +0530

    Updated GlobalUsing statements.

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,2 +1,10 @@
 ﻿global using Basket.API.Models;
 global using BuildingBlocks.CQRS;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using BuildingBlocks.Behaviours;
+global using BuildingBlocks.Exceptions;
+global using Marten;
+global using Basket.API.Exceptions;
