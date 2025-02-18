76e9b69475372b232282d64f35e8213df9aa902d
Darshil Shah <sdarshil786@gmail.com>
2025-02-18 23:10:28 +0530

    Auto Migrate and auto seed EF Core Entities to SQL Server when Application Startup

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -22,11 +22,11 @@
         {
             get
             {
-                var address1 = Address.Of("mehmet", "ozkaya", "mehmet@gmail.com", "Bahcelievler No:4", "Turkey", "Istanbul", "38050");
-                var address2 = Address.Of("john", "doe", "john@gmail.com", "Broadway No:1", "England", "Nottingham", "08050");
+                var address1 = Address.Of("darshil", "shah", "darshil@gmail.com", "some street", "India", "Gujarat", "38050");
+                var address2 = Address.Of("bhavesh", "shah", "bhavesh@gmail.com", "new street", "India", "Pune", "08050");
 
-                var payment1 = Payment.Of("mehmet", "5555555555554444", "12/28", "355", 1);
-                var payment2 = Payment.Of("john", "8885555555554444", "06/30", "222", 2);
+                var payment1 = Payment.Of("darshil", "5555555555554444", "12/28", "355", 1);
+                var payment2 = Payment.Of("bhavesh", "8885555555554444", "06/30", "222", 2);
 
                 var order1 = Order.Create(
                                 OrderId.Of(Guid.NewGuid()),
